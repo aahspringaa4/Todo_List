73f2e4f4bb092e29282a04b67f3033c99248f97b
유현명 <aahspringaa4@naver.com>
2023-06-14 11:01:13 +0900

    ⚡️ :: business logic

---
+++ app/src/main/java/com/example/todolistadexample/logic/data/AdValueExample.kt
@@ -0,0 +1,8 @@
+package com.example.show_ad.logic.data
+
+data class AdValueExample(
+    val title: String = "Dormitory Management System",
+    val content: String = "학생들을 위한 기숙사 관리 시스템",
+    val addIntroduce: String = "by DMS",
+    val image: String = "https://image-dms.s3.ap-northeast-2.amazonaws.com/2dbe672a-c03b-48a4-9399-8818cc1f125d%7C%7C20230423_220943.jpg"
+)
---
+++ app/src/main/java/com/example/todolistadexample/logic/domain/AdUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.show_ad.logic.domain
+
+import com.example.show_ad.logic.data.AdValueExample
+
+class AdUseCase {
+    fun excute() = AdValueExample()
+}
